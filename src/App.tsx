110fd07a54573d0f3c6e89cdf2bb21a19d4c8d31
David <twentytwentyaugust@outlook.com>
2023-12-14 03:57:56 +0900

    implemented basic ui & logic for the table

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Payout {
+  dateAndTime: string;
+  status: string;
+  value: string;
+  username: string;
 }
 
-export default App;
+interface Metadata {
+  page: number;
+  limit: number;
+  totalCount: number;
+}
+
+const PayoutHistoryPage: React.FC = () => {
+  const [payouts, setPayouts] = useState<Payout[]>([]);
+  const [metadata, setMetadata] = useState<Metadata>({ page: 1, limit: 10, totalCount: 0 });
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isSearch, setIsSearch] = useState(false);
+
+  useEffect(() => {
+    // Fetch payouts based on search criteria
+    const fetchPayouts = async () => {
+      try {
+        const response = await axios.get('https://theseus-staging.lithium.ventures/api/v1/analytics/tech-test/payouts', {
+          params: {
+            page: metadata.page,
+            limit: metadata.limit,
+            totalCount: metadata.totalCount,
+          },
+        });
+        setPayouts(response.data.data);
+        setMetadata(response.data.metadata);
+      } catch (error) {
+        console.error('Error fetching payouts:', error);
+      }
+    };
+
+    fetchPayouts();
+  }, [metadata.page]);
+
+
+  const handlePageChange = (newPage: number) => {
+    setMetadata({ ...metadata, page: newPage });
+  };
+
+  const handleSearchClick = () => {
+    setIsSearch(true);
+    // Trigger API request for search when the search button is clicked
+    setMetadata({ ...metadata, page: 1 }); // Reset pagination to first page
+  };
+
+  return (
+    <div>
+      {/* Filter section */}
+      <div>
+        <input type="text" placeholder="Search by User Name" onChange={(e) => setSearchTerm(e.target.value)} />
+        <button onClick={handleSearchClick}>Search</button>
+      </div>
+
+      {/* Payout history table */}
+      <table>
+        <thead>
+          <tr>
+            <th>User Name</th>
+            <th>Date & Time</th>
+            <th>Status</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          {payouts.map((payout, index) => (
+            <tr key={index}>
+              <td>{payout.username}</td>
+              <td>{payout.dateAndTime}</td>
+              <td>{payout.status}</td>
+              <td>{payout.value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {/* Pagination */}
+      <div>
+        <button disabled={metadata.page === 1} onClick={() => handlePageChange(1)}>First</button>
+        <button disabled={metadata.page === 1} onClick={() => handlePageChange(metadata.page - 1)}>Prev</button>
+        <span>{metadata.page}</span>
+        <button disabled={metadata.page === Math.ceil(metadata.totalCount / metadata.limit)} onClick={() => handlePageChange(metadata.page + 1)}>Next</button>
+        <button disabled={metadata.page === Math.ceil(metadata.totalCount / metadata.limit)} onClick={() => handlePageChange(Math.ceil(metadata.totalCount / metadata.limit))}>Last</button>
+      </div>
+    </div>
+  );
+};
+
+export default PayoutHistoryPage;
